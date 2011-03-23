faf19daa0ee2c1c81195c5ad9e934b71786deaae
hunterae <hunterae@gmail.com>
2011-03-22 22:10:30 -0400

    Added ability to specify multiple relationships simultaneously

--- has-relationship.gemspec
+++ has-relationship.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{has-relationship}
-  s.version = "0.0.1"
+  s.version = "0.0.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Andrew Hunter"]
@@ -16,8 +16,6 @@ Gem::Specification.new do |s|
     "README.rdoc"
   ]
   s.files = [
-    "Gemfile",
-    "Gemfile.lock",
     "README.rdoc",
     "Rakefile",
     "VERSION",
@@ -32,5 +30,15 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
   s.rubygems_version = %q{1.3.7}
   s.summary = %q{has_relationship is an easy way to instantly create an association between any two database tables}
+
+  if s.respond_to? :specification_version then
+    current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
+    s.specification_version = 3
+
+    if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
+    else
+    end
+  else
+  end
 end
 
--- lib/has_relationship/has_relationship.rb
+++ lib/has_relationship/has_relationship.rb
@@ -1,88 +1,96 @@
 module HasRelationship
   module Base    
-    def has_relationship(type, options={})
-      class_name = type.to_s.classify
+    def has_relationship(types, options={})
+      types = [types] unless types.kind_of?(Array)
+
+      types.each do |type|
+        class_name = type.to_s.classify
     
-      # default relationship: Class1_Class2, where Class1 is the class of this class, 
-      #   Class2 is the class of that this class has a relationship with
-      relationship = "#{self.to_s}_#{class_name}"    
+        # default relationship: Class1_Class2, where Class1 is the class of this class, 
+        #   Class2 is the class of that this class has a relationship with
+        relationship = "#{self.to_s}_#{class_name}"    
       
-      unless options[:singular].present?
-        as = options[:as] ? options[:as].to_s : type.to_s
+        unless options[:singular].present?
+          as = options[:as] ? options[:as].to_s : type.to_s
         
-        options[:singular] = (as.pluralize != as) 
-      end
-  
-      if options[:as]
-        resource_name = options[:as].to_sym
-        relationship = "#{self.to_s}_#{options[:as].to_s.classify}"
-      elsif options[:singular]
-        resource_name = type.to_s.tableize.singularize.to_sym
-      else
-        resource_name = type.to_s.tableize.to_sym
-      end
-    
-      relationship = options[:relationship] if options[:relationship] 
-      relationship_through = options[:singular] ? "relationship_through_#{relationship.to_s.tableize.singularize}".to_sym : "relationship_through_#{relationship.to_s.tableize}".to_sym
-
-      HasRelationship::Relationship.class_eval do
-        belongs_to "relation2_#{class_name.downcase}".to_sym,  :class_name => class_name, :foreign_key => "relation2_id"
-
-        before_validation do
-          self.relation2 = self.send("relation2_#{class_name.downcase}") unless self.relation2.present? or self.send("relation2_#{class_name.downcase}").nil?
+          options[:singular] = (as.pluralize != as) 
         end
-      end
-      
-      class_eval do
-        if options[:singular]
-          has_one relationship_through, :as => :relation1, :dependent => :destroy, :class_name => "HasRelationship::Relationship", :conditions => {:relationship => relationship}
-          has_one resource_name, :through => relationship_through, :class_name => class_name, :source => "relation2_#{class_name.downcase}".to_s, :conditions => ["relationships.relation2_type = ? and relationships.relationship = ?", class_name, relationship]
+  
+        if options[:as]
+          resource_name = options[:as].to_sym
+          relationship = "#{self.to_s}_#{options[:as].to_s.classify}"
+        elsif options[:singular]
+          resource_name = type.to_s.tableize.singularize.to_sym
         else
-          has_many relationship_through, :as => :relation1, :dependent => :destroy, :class_name => "HasRelationship::Relationship", :conditions => {:relationship => relationship}
-          has_many resource_name, :through => relationship_through, :class_name => class_name, :source => "relation2_#{class_name.downcase}".to_sym, :conditions => ["relationships.relation2_type = ? and relationships.relationship = ?", class_name, relationship]
+          resource_name = type.to_s.tableize.to_sym
+        end
+    
+        relationship = options[:relationship] if options[:relationship] 
+        relationship_through = options[:singular] ? "relationship_through_#{relationship.to_s.tableize.singularize}".to_sym : "relationship_through_#{relationship.to_s.tableize}".to_sym
+
+        HasRelationship::Relationship.class_eval do
+          belongs_to "relation2_#{class_name.downcase}".to_sym,  :class_name => class_name, :foreign_key => "relation2_id"
+
+          before_validation do
+            self.relation2 = self.send("relation2_#{class_name.downcase}") unless self.relation2.present? or self.send("relation2_#{class_name.downcase}").nil?
+          end
+        end
+      
+        class_eval do
+          if options[:singular]
+            has_one relationship_through, :as => :relation1, :dependent => :destroy, :class_name => "HasRelationship::Relationship", :conditions => {:relationship => relationship}
+            has_one resource_name, :through => relationship_through, :class_name => class_name, :source => "relation2_#{class_name.downcase}".to_s, :conditions => ["relationships.relation2_type = ? and relationships.relationship = ?", class_name, relationship]
+          else
+            has_many relationship_through, :as => :relation1, :dependent => :destroy, :class_name => "HasRelationship::Relationship", :conditions => {:relationship => relationship}
+            has_many resource_name, :through => relationship_through, :class_name => class_name, :source => "relation2_#{class_name.downcase}".to_sym, :conditions => ["relationships.relation2_type = ? and relationships.relationship = ?", class_name, relationship]
+          end
         end
       end
     end
     
-    def has_inverse_relationship(type, options={})
-      class_name = type.to_s.classify
+    def has_inverse_relationship(types, options={})
+      types = [types] unless types.kind_of?(Array)
+
+      types.each do |type|
+        class_name = type.to_s.classify
     
-      # default relationship: Class2_Class1, where Class1 is the class of this class, 
-      #   Class2 is the class of that this class has a relationship with
-      relationship = "#{class_name}_#{self.to_s}"
+        # default relationship: Class2_Class1, where Class1 is the class of this class, 
+        #   Class2 is the class of that this class has a relationship with
+        relationship = "#{class_name}_#{self.to_s}"
       
-      unless options[:singular].present?
-        as = options[:as] ? options[:as].to_s : type.to_s
-        options[:singular] = (as.pluralize != as) 
-      end
-  
-      if options[:as]
-        resource_name = options[:as].to_sym
-        relationship = "#{options[:as].to_s.classify}_#{self.to_s}"
-      elsif options[:singular]
-        resource_name = type.to_s.tableize.singularize.to_sym
-      else
-        resource_name = type.to_s.tableize.to_sym
-      end
-    
-      relationship = options[:relationship] if options[:relationship] 
-      relationship_through = options[:singular] ? "inverse_relationship_through_#{relationship.to_s.tableize.singularize}".to_sym : "inverse_relationship_through_#{relationship.to_s.tableize}".to_sym
-
-      HasRelationship::Relationship.class_eval do
-        belongs_to "relation1_#{class_name.downcase}".to_sym,  :class_name => class_name, :foreign_key => "relation1_id"
-
-        before_validation do
-          self.relation1 = self.send("relation1_#{class_name.downcase}") unless self.relation1.present? or self.send("relation1_#{class_name.downcase}").nil?
+        unless options[:singular].present?
+          as = options[:as] ? options[:as].to_s : type.to_s
+          options[:singular] = (as.pluralize != as) 
         end
-      end
-      
-      class_eval do
-        if options[:singular]
-          has_one relationship_through, :as => :relation2, :dependent => :destroy, :class_name => "HasRelationship::Relationship", :conditions => {:relationship => relationship}
-          has_one resource_name, :through => relationship_through, :class_name => class_name, :source => "relation1_#{class_name.downcase}".to_sym, :conditions => ["relationships.relation1_type = ? and relationships.relationship = ?", class_name, relationship]
+  
+        if options[:as]
+          resource_name = options[:as].to_sym
+          relationship = "#{options[:as].to_s.classify}_#{self.to_s}"
+        elsif options[:singular]
+          resource_name = type.to_s.tableize.singularize.to_sym
         else
-          has_many relationship_through, :as => :relation2, :dependent => :destroy, :class_name => "HasRelationship::Relationship", :conditions => {:relationship => relationship}
-          has_many resource_name, :through => relationship_through, :class_name => class_name, :source => "relation1_#{class_name.downcase}".to_sym, :conditions => ["relationships.relation1_type = ? and relationships.relationship = ?", class_name, relationship]
+          resource_name = type.to_s.tableize.to_sym
+        end
+    
+        relationship = options[:relationship] if options[:relationship] 
+        relationship_through = options[:singular] ? "inverse_relationship_through_#{relationship.to_s.tableize.singularize}".to_sym : "inverse_relationship_through_#{relationship.to_s.tableize}".to_sym
+
+        HasRelationship::Relationship.class_eval do
+          belongs_to "relation1_#{class_name.downcase}".to_sym,  :class_name => class_name, :foreign_key => "relation1_id"
+
+          before_validation do
+            self.relation1 = self.send("relation1_#{class_name.downcase}") unless self.relation1.present? or self.send("relation1_#{class_name.downcase}").nil?
+          end
+        end
+      
+        class_eval do
+          if options[:singular]
+            has_one relationship_through, :as => :relation2, :dependent => :destroy, :class_name => "HasRelationship::Relationship", :conditions => {:relationship => relationship}
+            has_one resource_name, :through => relationship_through, :class_name => class_name, :source => "relation1_#{class_name.downcase}".to_sym, :conditions => ["relationships.relation1_type = ? and relationships.relationship = ?", class_name, relationship]
+          else
+            has_many relationship_through, :as => :relation2, :dependent => :destroy, :class_name => "HasRelationship::Relationship", :conditions => {:relationship => relationship}
+            has_many resource_name, :through => relationship_through, :class_name => class_name, :source => "relation1_#{class_name.downcase}".to_sym, :conditions => ["relationships.relation1_type = ? and relationships.relationship = ?", class_name, relationship]
+          end
         end
       end
     end
